8846d37dc5fa7cc298b733bae86c66e9a8241e53
chiaramery <meringolachiara@gmail.com>
2023-01-09 16:32:59 +0100

    Push

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,6 +2,9 @@
 @section('content')
     <div class="container">
         <h2>Lista dei comics</h2>
+        <a class="btn btn-danger" href="{{ route('comics.create') }}">
+            <i class="fa-solid fa-plus"></i>
+        </a>
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -18,7 +21,20 @@
                         <td>{{ $comic->series }}</td>
                         <td>{{ $comic->type }}</td>
                         <td>
-                            <a class="btn btn-primary" href="{{ route('comics.show', $comic->id) }}">Dettagli</a>
+                            <a class="btn btn-primary" href="{{ route('comics.show', $comic->id) }}">
+                                <i class="fa-solid fa-info"></i>
+                            </a>
+                            <a class="btn btn-warning" href="{{ route('comics.show', $comic->id) }}">
+                                <i class="fa-solid fa-pen"></i>
+                            </a>
+                            <form action="{{route('comics.destroy', $comic->id)}}" class="d-inline" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button class="btn btn-danger">
+                                <i class="fa-regular fa-trash-can"></i>
+                            </button>
+                        </form>
+
                         </td>
                     </tr>
                 @endforeach
